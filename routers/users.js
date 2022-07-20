b7e8d928403226e2389ddd5a18889d8bbda742d2
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-20 16:44:47 +0300

    Adding endpoints

--- routers/users.js
+++ routers/users.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const db = require("../db/users");
 const usersRouter = express.Router();
+
+usersRouter.get("/", db.getUsers);
+usersRouter.get("/:id", db.getUserById);
+usersRouter.put("/:id", db.updateUser);
 
 module.exports = usersRouter;
