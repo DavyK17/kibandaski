cad3b49e95818779a7117b9fb9fac863c64b59ec
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-20 16:42:50 +0300

    Initialising database file

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const users = require("./users");
+// const products = require("./products");
+// const orders = require("./orders");
+// const cart = require("./cart");
+
+module.exports = {
+    users,
+    // products,
+    // orders,
+    // cart
+}
