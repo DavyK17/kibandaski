5c5d9d671abf99e2fa26954cb0b88f81012e14b6
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-20 16:45:01 +0300

    Adding user endpoints

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 8000;
+const db = require("./db/index");
 
 // App
 app.use(bodyParser.json());
@@ -13,7 +14,23 @@ app.get("/", (req, res) => {
     res.json({ info: "Welcome to Kibandaski" });
 });
 
+// User registration
+app.post("/register", db.users.createUser);
+
+// Routers
+const usersRouter = require("./routers/users");
+app.use("/users", usersRouter);
+
+// const productsRouter = require("./routers/products");
+// app.use("/products", productsRouter);
+
+// const ordersRouter = require("./routers/orders");
+// app.use("/orders", ordersRouter);
+
+// const cartRouter = require("./routers/cart");
+// app.use("/cart", cartRouter);
+
 // Listener
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}.`);
 });
