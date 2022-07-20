508734685d42dfd52f13731adf4b48f843418151
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-20 16:42:24 +0300

    Additional config

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
+
+// App
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Wagwan</h1>");
+    res.json({ info: "Welcome to Kibandaski" });
 });
 
+// Listener
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
