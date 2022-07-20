887e0d8401fada1b07bc6bf1bdbd489177930f49
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-20 16:59:13 +0300

    Refactoring with async/await

--- db/users.js
+++ db/users.js
@@ -10,64 +10,62 @@ const pool = new Pool({
     database: process.env.DB_DATABASE
 });
 
-const getUsers = (req, res) => {
-    pool.query(
-        "SELECT * FROM users ORDER BY id ASC",
-        (err, result) => {
-            if (err) throw err;
-            res.status(200).json(result.rows);
-        }
-    );
+const getUsers = async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users ORDER BY id ASC");
+        res.status(200).json(result.rows);
+    } catch(err) {
+        throw err;
+    }
 }
 
-const getUserById = (req, res) => {
+const getUserById = async (req, res) => {
     const id = parseInt(req.params.id);
 
-    pool.query(
-        "SELECT * FROM users WHERE id = $1", [id],
-        (err, result) => {
-            if (err) throw err;
-            res.status(200).json(result.rows);
-        }
-    );
+    try {
+        const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+        res.status(200).json(result.rows);
+    } catch(err) {
+        throw err;
+    }
 }
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
     const { firstName, lastName, email } = req.body;
+    let text = `INSERT INTO users (id, first_name, last_name, email, created_at) VALUES (${parseInt(idGen(7))}, $1, $2, $3, to_timestamp(${Date.now()} / 1000)) RETURNING *`;
+    let values = [firstName, lastName, email];
 
-    pool.query(
-        `INSERT INTO users (id, first_name, last_name, email, created_at) VALUES (${parseInt(idGen(7))}, $1, $2, $3, to_timestamp(${Date.now()} / 1000)) RETURNING *;`, [firstName, lastName, email],
-        (err, result) => {
-            if (err) throw err;
-            res.status(201).send(`User added with ID: ${result.rows[0].id}`);
-        }
-    );
+    try {
+        const result = await pool.query(text, values);
+        res.status(201).send(`User added with ID: ${result.rows[0].id}`);
+    } catch(err) {
+        throw err;
+    }
 }
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
     const id = parseInt(req.params.id);
     const { firstName, lastName, email } = req.body;
+    let text = "UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4";
+    let values = [firstName, lastName, email, id];
 
-    pool.query(
-        "UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4",
-        [firstName, lastName, email, id],
-        (err, result) => {
-            if (err) throw err;
-            res.status(200).send(`User modified with ID: ${id}`);
-        }
-    );
+    try {
+        const result = await pool.query(text, values);
+        res.status(200).send(`User modified with ID: ${id}`);
+    } catch(err) {
+        throw err;
+    }
 }
 
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
     const id = parseInt(req.params.id);
 
-    pool.query(
-        "DELETE FROM users WHERE id = $1", [id],
-        (err, result) => {
-            if (err) throw err;
-            res.status(204).send(`User deleted with ID: ${id}`);
-        }
-    )
+    try {
+        const result = await pool.query("DELETE FROM users WHERE id = $1", [id]);
+        res.status(204).send(`User deleted with ID: ${id}`);
+    } catch(err) {
+        throw err;
+    }
 }
 
 module.exports = {
