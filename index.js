9ee254d6f946865caf7ff9468ad0e478a2aaae3f
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-21 19:56:57 +0300

    Moving authentication logic to separate router

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
+/* CONFIGURATION */
+
+// General
 require("dotenv").config();
-const express = require("express");
+const app = require("express")();
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
+const port = process.env.PORT || 8000;
 
-const { Pool } = require("pg");
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-
+// Express session
 const session = require("express-session");
 const store = new session.MemoryStore();
-
-const app = express();
-const db = require("./db/index");
-const port = process.env.PORT || 8000;
 
 // Body Parser
 app.use(bodyParser.json());
@@ -32,78 +22,27 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 172800000,
-        secure: true,
+        secure: false,
         sameSite: "none",
     },
     store
 }));
 
-// Passport configuration
+// Passport.js
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-    return done(null, user.id);
-});
 
-passport.deserializeUser(async (id, done) => {
-    try {
-        const result = await pool.query("SELECT id FROM users WHERE id = $1", [id]);
-        if (result.rows.length === 0) return done(null, false);
-
-        return done(null, { id: result.rows[0].id });
-    } catch(err) {
-        return done(err);
-    }
-});
-
-passport.use(new LocalStrategy(
-    { usernameField: "email" },
-    async (email, password, done) => {
-        try {
-            const result = await pool.query("SELECT id, email, password FROM users WHERE email = $1", [email]);
-            if (result.rows.length === 0) return done(null, false);
-            
-            const passwordMatch = await bcrypt.compare(password, result.rows[0].password);
-            if (!passwordMatch) return done(null, false);
-
-            return done(null, { id: result.rows[0].id });
-        } catch(err) {
-            return done(err);
-        }
-    }
-));
+/* IMPLEMENTATION */
 
 app.get("/", (req, res) => {
     res.json({ info: "Welcome to Kibandaski" });
 });
 
-// User account
-app.get("/account", (req, res) => {
-    res.json({ info: "Login successful" });
-});
-
-// User registration
-app.get("/register", (req, res) => {
-    res.json({ info: "Create a new account" });
-});
-
-app.post("/register", db.users.createUser);
-
-// User login
-app.get("/login", (req, res) => {
-    res.json({ info: "Kindly log in with your account details" });
-});
-
-app.post(
-    "/login",
-    passport.authenticate("local", { failureRedirect: "/login" }),
-    (req, res) => {
-        res.redirect("account")
-    }
-);
-
 // Routers
+const authRouter = require("./routers/auth");
+app.use("/", authRouter);
+
 const usersRouter = require("./routers/users");
 app.use("/users", usersRouter);
 
