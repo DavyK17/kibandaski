1ff14a09f6bcf31dee9e72c3c288be29f6e1407e
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-26 23:09:58 +0300

    Removing ensureLoggedIn

--- routers/users.js
+++ routers/users.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const db = require("../db/users");
-const { ensureLoggedIn } = require("connect-ensure-login");
 
 router.get("/", db.getUsers);
 router.get("/:id", db.getUserById);
