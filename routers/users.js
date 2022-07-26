d7d944dad74e3b0fe51966e773f79166bcad3b0a
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-26 22:08:22 +0300

    Removing update and delete endpoints

--- routers/users.js
+++ routers/users.js
@@ -4,7 +4,5 @@ const { ensureLoggedIn } = require("connect-ensure-login");
 
 router.get("/", db.getUsers);
 router.get("/:id", db.getUserById);
-router.put("/:id", ensureLoggedIn("/login"), db.updateUser);
-router.delete("/:id", ensureLoggedIn("/login"), db.deleteUser);
 
 module.exports = router;
