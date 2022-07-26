569822d6fedf0d10fbaa8587078525dd5f367df7
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-26 20:47:51 +0300

    Adding endpoints

--- routers/products.js
+++ routers/products.js
@@ -1,2 +1,7 @@
 const router = require("express").Router();
+const db = require("../db/products");
+
+router.get("/", db.getProducts);
+router.get("/:id", db.getProductById);
+
 module.exports = router;
