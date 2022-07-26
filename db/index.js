abb8f641208c108dcd281492bb8ededfddaea99d
Davy Kamanzi <davykamanzi@gmail.com>
2022-07-26 20:47:42 +0300

    Importing products

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const users = require("./users");
-// const products = require("./products");
+const products = require("./products");
 // const orders = require("./orders");
 // const cart = require("./cart");
 
 module.exports = {
     users,
-    // products,
+    products,
     // orders,
     // cart
 }
