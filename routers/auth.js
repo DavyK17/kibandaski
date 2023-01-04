9a5eb12f07484607b626ab32b613a641f5d5c933
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-04 14:28:03 +0300

    Adding role to user object

--- routers/auth.js
+++ routers/auth.js
@@ -4,33 +4,24 @@
 require("dotenv").config();
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
+const client = require("../db/client");
 const db = require("../db/index");
 
 // connect-ensure-login
 const login = require("connect-ensure-login").ensureLoggedIn("/login");
 const logout = require("connect-ensure-login").ensureLoggedOut("/account");
 
-// node-postgres
-const { Pool } = require("pg");
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-
 // Passport.js
 const passport = require("passport");
 const authenticate = async(email, password, done) => {
     try {
-        let result = await pool.query("SELECT users.id AS id, users.email AS email, users.password AS password, carts.id AS cart_id FROM users JOIN carts ON carts.user_id = users.id WHERE email = $1", [email]);
+        let result = await client.query("SELECT users.id AS id, users.email AS email, users.password AS password, users.role AS role, carts.id AS cart_id FROM users JOIN carts ON carts.user_id = users.id WHERE email = $1", [email]);
         if (result.rows.length === 0) return done(null, false);
 
         const passwordMatch = await bcrypt.compare(password, result.rows[0].password);
         if (!passwordMatch) return done(null, false);
 
-        return done(null, { id: result.rows[0].id, email: result.rows[0].email, cartId: result.rows[0].cart_id });
+        return done(null, { id: result.rows[0].id, email: result.rows[0].email, role: result.rows[0].role, cartId: result.rows[0].cart_id });
     } catch (err) {
         return done(err);
     }
@@ -48,10 +39,10 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async(id, done) => {
     try {
-        let result = await pool.query("SELECT users.id AS id, users.email AS email, carts.id AS cart_id FROM users JOIN carts ON carts.user_id = users.id WHERE users.id = $1", [id]);
+        let result = await client.query("SELECT users.id AS id, users.email AS email, users.role AS role, carts.id AS cart_id FROM users JOIN carts ON carts.user_id = users.id WHERE users.id = $1", [id]);
         if (result.rows.length === 0) return done(null, false);
 
-        return done(null, { id: result.rows[0].id, email: result.rows[0].email, cartId: result.rows[0].cart_id });
+        return done(null, { id: result.rows[0].id, email: result.rows[0].email, role: result.rows[0].role, cartId: result.rows[0].cart_id });
     } catch (err) {
         return done(err);
     }
