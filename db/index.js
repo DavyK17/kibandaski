c8e8858f6fb480f2b5d3f7077202f4641e16355c
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-04 14:57:58 +0300

    Fixing exports

--- db/index.js
+++ db/index.js
@@ -1,11 +1,27 @@
-const users = require("./users");
-const products = require("./products");
-const orders = require("./orders");
-const cart = require("./cart");
+// ADMIN FUNCTIONS
+const loginAttempts = require("./admin/loginAttempts");
+const ordersA = require("./admin/orders");
+const productsA = require("./admin/products");
+const usersA = require("./admin/users");
 
+// CUSTOMER FUNCTIONS
+const cart = require("./customer/cart");
+const ordersC = require("./customer/orders");
+const productsC = require("./customer/products");
+const usersC = require("./customer/users");
+
+// EXPORTS
 module.exports = {
-    users,
-    products,
-    orders,
-    cart
+    admin: {
+        loginAttempts,
+        orders: ordersA,
+        products: productsA,
+        users: usersA
+    },
+    customer: {
+        cart,
+        orders: ordersC,
+        products: productsC,
+        users: usersC
+    }
 }
