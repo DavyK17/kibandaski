42732edfec4c3f26313ebf9598fc2256219aa80f
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-04 23:13:57 +0300

    Replacing ID route with category route

--- routers/customer/products.js
+++ routers/customer/products.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const db = require("../../db/index").customer.products;
 
 router.get("/", db.getProducts);
-router.get("/:id", db.getProductById);
+router.get("/:category", db.getProductsByCategory);
 
 module.exports = router;
