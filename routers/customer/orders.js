9850bf5d0f312383c27ac431c09746d4db831959
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-05 00:06:55 +0300

    Fixing routes

--- routers/customer/orders.js
+++ routers/customer/orders.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const db = require("../../db/index").customer.orders;
 
 router.get("/", db.getOrders);
-router.get("/:id", db.getOrderById);
-router.delete("/:id", db.cancelOrder);
+router.delete("/", db.cancelOrder);
 
 module.exports = router;
