4e4d25a845b8cc4289f3469bc27e186d9ec418ab
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-05 00:06:50 +0300

    Getting order by ID and cancelling order with query

--- db/customer/orders.js
+++ db/customer/orders.js
@@ -3,43 +3,37 @@ const client = require("../client");
 
 const getOrders = async(req, res) => {
     try {
-        let result = await client.query("SELECT id, status FROM orders WHERE user_id = $1 ORDER BY created_at DESC", [req.user.id]);
-        res.status(200).json(result.rows);
-    } catch (err) {
-        res.status(500).send(`Error: ${err.detail}`);
-    }
-}
+        if (req.query.id) { // Get order by ID
+            let result = await client.query("SELECT id, status FROM orders WHERE id = $1 AND user_id = $2", [req.query.id, req.user.id]);
+            if (result.rows.length === 0) return res.status(404).send("Error: This order does not exist.");
 
-const getOrderById = async(req, res) => {
-    try {
-        let result = await client.query("SELECT id, status FROM orders WHERE id = $1 AND user_id = $2", [req.params.id, req.user.id]);
-        if (result.rows.length === 0) return res.status(404).send("Error: This order does not exist.");
+            let order = {...result.rows[0], items: [] };
 
-        let order = {...result.rows[0], items: [] };
+            result = await client.query("SELECT order_items.product_id AS product_id, order_items.quantity AS quantity, (order_items.quantity * products.price) AS total_cost FROM order_items JOIN products ON order_items.product_id = products.id WHERE order_items.order_id = $1", [req.query.id]);
+            result.rows.forEach(({ product_id, quantity, total_cost }) => {
+                let item = { productId: product_id, quantity, totalCost: total_cost };
+                order.items.push(item);
+            });
 
-        result = await client.query("SELECT order_items.product_id AS product_id, order_items.quantity AS quantity, (order_items.quantity * products.price) AS total_cost FROM order_items JOIN products ON order_items.product_id = products.id WHERE order_items.order_id = $1", [req.params.id]);
-        result.rows.forEach(({ product_id, quantity, total_cost }) => {
-            let item = { productId: product_id, quantity, totalCost: total_cost };
-            order.items.push(item);
-        });
-
-        res.status(200).json(order);
+            res.status(200).json(order);
+        } else { // Get all orders
+            let result = await client.query("SELECT id, status FROM orders WHERE user_id = $1 ORDER BY created_at DESC", [req.user.id]);
+            res.status(200).json(result.rows);
+        }
     } catch (err) {
         res.status(500).send(`Error: ${err.detail}`);
     }
 }
 
 const cancelOrder = async(req, res) => {
+    if (!req.query.id) return res.status(400).send("Error: No order ID specified");
+
     try {
-        let result = await client.query("UPDATE orders SET status = 'cancelled' WHERE id = $1 AND user_id = $2 RETURNING id", [req.params.id, req.user.id]);
+        let result = await client.query("UPDATE orders SET status = 'cancelled' WHERE id = $1 AND user_id = $2 RETURNING id", [req.query.id, req.user.id]);
         res.status(200).send(`Order cancelled with ID: ${result.rows[0].id}`);
     } catch (err) {
         res.status(500).send(`Error: ${err.detail}`);
     }
 }
 
-module.exports = {
-    getOrders,
-    getOrderById,
-    cancelOrder
-}
+module.exports = { getOrders, cancelOrder };
