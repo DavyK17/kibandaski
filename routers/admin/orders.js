e344a5bafec655ffacba57175b1eb4f67e569784
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-04 23:13:02 +0300

    Fixing fulfill route

--- routers/admin/orders.js
+++ routers/admin/orders.js
@@ -3,7 +3,6 @@ const db = require("../../db/index").admin.orders;
 
 router.get("/", db.getOrders);
 router.get("/:userId", db.getOrdersByUser);
-router.get("/:orderId", db.getOrderById);
-router.get("/fulfill/:orderId", db.fulfillOrder);
+router.get("/fulfill", db.fulfillOrder);
 
 module.exports = router;
