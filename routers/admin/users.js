d9cdc67661acbf2ace00e691e9776340fe7579f8
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-04 23:02:13 +0300

    Moving users route to admin router

--- routers/admin/users.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const db = require("../../db/index").admin.users;
-
-router.get("/", db.getUsers);
-router.get("/:id", db.getUserById);
-
-module.exports = router;
