d9a2efaaad6784d1f64847849452e1545fc7c641
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-05 09:41:40 +0300

    Adding order acknowledgement route

--- routers/admin/orders.js
+++ routers/admin/orders.js
@@ -3,6 +3,7 @@ const db = require("../../db/index").admin.orders;
 
 router.get("/", db.getOrders);
 router.get("/:userId", db.getOrdersByUser);
+router.get("/acknowledge", db.acknowledgeOrder);
 router.get("/fulfill", db.fulfillOrder);
 
 module.exports = router;
