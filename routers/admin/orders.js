3e0e41382282104ced2e42c2ac90e4feb5b84519
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-05 10:28:03 +0300

    Fixing route order

--- routers/admin/orders.js
+++ routers/admin/orders.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const db = require("../../db/index").admin.orders;
 
-router.get("/", db.getOrders);
-router.get("/:userId", db.getOrdersByUser);
 router.get("/acknowledge", db.acknowledgeOrder);
 router.get("/fulfill", db.fulfillOrder);
+
+router.get("/", db.getOrders);
+router.get("/:userId", db.getOrdersByUser);
 
 module.exports = router;
