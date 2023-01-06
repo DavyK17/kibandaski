532df49f77b9f7201a17470926959ed0ecee5933
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-06 11:18:41 +0300

    Refactoring

--- util/idGen.js
+++ util/idGen.js
@@ -1,11 +1,9 @@
 const idGen = length => {
     const chars = "0123456789";
     const charLength = chars.length;
-    let result = "";
 
-    for (let i = 0; i < length; i++) {
-        result += chars.charAt(Math.floor(Math.random() * charLength));
-    }
+    let result = "";
+    for (let i = 0; i < length; i++) result += chars.charAt(Math.floor(Math.random() * charLength));
     return result;
 }
 
