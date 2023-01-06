2c9641c043dd517127afe1aa0f48946179b35cc0
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-06 11:14:05 +0300

    Minor formatting edit

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ app.use(passport.session());
 // Swagger UI Express
 const swaggerUI = require("swagger-ui-express");
 const swaggerDocument = require("./openapi.json");
-
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
