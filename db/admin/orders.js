3b4f6938fce34df0c26f6df11804bed60c2e0129
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-07 11:19:20 +0300

    Adding created at time to get functions

--- db/admin/orders.js
+++ db/admin/orders.js
@@ -11,11 +11,11 @@ const getOrders = async(req, res) => {
             if (!isNumeric(id, { no_symbols: true }) || !isLength(id, { min: 10, max: 10 })) return res.status(400).send("Error: Invalid order ID provided.");
 
             // Send error if order does not exist
-            let result = await pool.query("SELECT id, user_id, status FROM orders WHERE id = $1", [id]);
+            let result = await pool.query("SELECT id, user_id, created_at, status FROM orders WHERE id = $1", [id]);
             if (result.rows.length === 0) return res.status(404).send("Error: This order does not exist.");
 
             // Create order object
-            let order = { id: result.rows[0].id, userId: result.rows[0].user_id, status: result.rows[0].status, items: [] };
+            let order = { id: result.rows[0].id, userId: result.rows[0].user_id, createdAt: result.rows[0].created_at, status: result.rows[0].status, items: [] };
 
             // Add each item on the order to array in order object
             result = await pool.query("SELECT order_items.product_id AS product_id, order_items.quantity AS quantity, (order_items.quantity * products.price) AS total_cost FROM order_items JOIN products ON order_items.product_id = products.id WHERE order_items.order_id = $1", [id]);
@@ -27,8 +27,23 @@ const getOrders = async(req, res) => {
             // Send order
             res.status(200).json(order);
         } else { // GET ALL ORDERS
-            let result = await pool.query("SELECT id, user_id, status FROM orders ORDER BY created_at DESC");
-            res.status(200).json(result.rows);
+            // Create orders array
+            let orders = [];
+
+            // Get orders
+            let result = await pool.query("SELECT id, user_id, created_at, status FROM orders ORDER BY created_at DESC");
+
+            // Send error if no orders found
+            if (result.rows.length === 0) return res.status(404).send("Error: No orders found.");
+
+            // Add each order to orders array
+            result.rows.forEach(({ id, user_id, created_at, status }) => {
+                let order = { id, userId: user_id, createdAt: created_at, status };
+                orders.push(order);
+            });
+
+            // Send orders array
+            res.status(200).json(orders);
         }
     } catch (err) {
         res.status(500).send("An unknown error occurred. Kindly try again.");
@@ -40,14 +55,24 @@ const getOrdersByUser = async(req, res) => {
     let id = trim(req.params.userId);
     if (!isNumeric(id, { no_symbols: true }) || !isLength(id, { min: 7, max: 7 })) return res.status(400).send("Error: Invalid user ID provided.");
 
-    try { // Get orders by user
-        let result = await pool.query("SELECT id, status FROM orders WHERE user_id = $1 ORDER BY created_at DESC", [id]);
+    try { // GET ORDERS BY USER
+        // Create orders array
+        let orders = [];
+
+        // Get orders by user
+        let result = await pool.query("SELECT id, created_at, status FROM orders WHERE user_id = $1 ORDER BY created_at DESC", [id]);
 
         // Send error if no orders found by user
         if (result.rows.length === 0) return res.status(404).send("Error: No orders found from provided user.");
 
+        // Add each order to orders array
+        result.rows.forEach(({ id, created_at, status }) => {
+            let order = { id, createdAt: created_at, status };
+            orders.push(order);
+        });
+
         // Send orders by user
-        res.status(200).json(result.rows);
+        res.status(200).json(orders);
     } catch (err) {
         res.status(500).send("An unknown error occurred. Kindly try again.");
     }
--- db/customer/orders.js
+++ db/customer/orders.js
@@ -15,11 +15,11 @@ const getOrders = async(req, res) => {
             if (!isNumeric(id, { no_symbols: true }) || !isLength(id, { min: 10, max: 10 })) return res.status(400).send("Error: Invalid order ID provided.");
 
             // Send error if order does not exist
-            let result = await pool.query("SELECT id, status FROM orders WHERE id = $1 AND user_id = $2", [id, userId]);
+            let result = await pool.query("SELECT id, created_at, status FROM orders WHERE id = $1 AND user_id = $2", [id, userId]);
             if (result.rows.length === 0) return res.status(404).send("Error: This order does not exist.");
 
             // Create order object
-            let order = {...result.rows[0], items: [] };
+            let order = { id: result.rows[0].id, createdAt: result.rows[0].created_at, status: result.rows[0].status, items: [] };
 
             // Add each item on order to items array in order object
             result = await pool.query("SELECT order_items.product_id AS product_id, order_items.quantity AS quantity, (order_items.quantity * products.price) AS total_cost FROM order_items JOIN products ON order_items.product_id = products.id WHERE order_items.order_id = $1", [id]);
@@ -31,8 +31,23 @@ const getOrders = async(req, res) => {
             // Send order
             res.status(200).json(order);
         } else { // GET ALL ORDERS
-            let result = await pool.query("SELECT id, status FROM orders WHERE user_id = $1 ORDER BY created_at DESC", [userId]);
-            res.status(200).json(result.rows);
+            // Create orders array
+            let orders = [];
+
+            // Get orders
+            let result = await pool.query("SELECT id, created_at, status FROM orders WHERE user_id = $1 ORDER BY created_at DESC", [userId]);
+
+            // Send error if no orders found
+            if (result.rows.length === 0) return res.status(404).send("Error: No orders found.");
+
+            // Add each order to orders array
+            result.rows.forEach(({ id, created_at, status }) => {
+                let order = { id, createdAt: created_at, status };
+                orders.push(order);
+            });
+
+            // Send orders array
+            res.status(200).json(orders);
         }
     } catch (err) {
         res.status(500).send("An unknown error occurred. Kindly try again.");
@@ -54,7 +69,7 @@ const cancelOrder = async(req, res) => {
 
     // CANCEL ORDER
     try { // Get order
-        let result = await pool.query("SELECT id, user_id, status FROM orders WHERE id = $1 AND user_id = $2", [id, userId]);
+        let result = await pool.query("SELECT * FROM orders WHERE id = $1 AND user_id = $2", [id, userId]);
 
         // Send error if order does not exist
         if (result.rows.length === 0) return res.status(404).send("Error: This order does not exist.");
