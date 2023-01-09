90b044e1db534976cb15f7443043acb0890371c4
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-09 15:14:03 +0300

    Restoring Passport

--- db/auth.js
+++ db/auth.js
@@ -1,6 +1,5 @@
 // IMPORTS
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const pool = require("./pool");
 const requestIP = require("request-ip");
 
@@ -9,7 +8,14 @@ const { isEmail, trim, escape, normalizeEmail } = require("validator");
 const sanitizeHtml = require("../util/sanitizeHtml");
 
 // FUNCTIONS
-const login = async(req, res) => {
+const logout = (req, res) => {
+    req.logout(err => {
+        if (err) return res.status(500).send("An unknown error occurred. Kindly try again.");
+        res.send("Logout successful");
+    });
+}
+
+const loginLocal = async(req, email, password, done) => {
     // Send error if already logged in
     if (req.user) return res.status(403).send("Error: You are already logged in.");
 
@@ -25,8 +31,6 @@ const login = async(req, res) => {
     }
 
     // Validate and sanitise email
-    let { email, password } = req.body;
-
     if (typeof email !== "string") return res.status(400).send("Error: Email must be a string.");
     email = sanitizeHtml(normalizeEmail(trim(escape(email)), { gmail_remove_dots: false }));
     if (!isEmail(email)) return res.status(400).send("Error: Invalid email provided.");
@@ -34,42 +38,25 @@ const login = async(req, res) => {
     try { // Get user details
         let result = await pool.query("SELECT users.id AS id, users.email AS email, users.password AS password, users.role AS role, carts.id AS cart_id FROM users JOIN carts ON carts.user_id = users.id WHERE email = $1", [email]);
 
-        // Send error if user does not exist
+        // Send null if user does not exist
         if (result.rows.length === 0) {
             await logAttempt(false);
-            return res.status(400).send("Error: Incorrect email or password provided.")
+            return done(null, false);
         };
 
-        // Send error if password hashes do not match
+        // Send null if password hashes do not match
         const passwordMatch = await bcrypt.compare(password, result.rows[0].password);
         if (!passwordMatch) {
             await logAttempt(false);
-            return res.status(400).send("Error: Incorrect email or password provided.")
+            return done(null, false);
         };
 
-        // Create user object
-        let user = { id: result.rows[0].id, email: result.rows[0].email, role: result.rows[0].role, cartId: result.rows[0].cart_id };
-
-        // Create JSON web token
-        let expiresIn = 172800;
-        const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn });
-
-        // Log successful login and confirm
+        // Add user to session
         await logAttempt(true);
-        res.status(200).json({ token });
+        return done(null, { id: result.rows[0].id, email: result.rows[0].email, role: result.rows[0].role, cartId: result.rows[0].cart_id });
     } catch (err) {
         res.status(500).send("An unknown error occurred. Kindly try again.");
     }
 }
 
-const logout = (req, res) => {
-    try {
-        req.user = null;
-        res.cookie("token", "", { maxAge: 0 });
-        res.status(200).send("Logout successful");
-    } catch (err) {
-        res.status(500).send("An unknown error occurred. Kindly try again.");
-    }
-}
-
-module.exports = { login, logout };
+module.exports = { logout, loginLocal };
