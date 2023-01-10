d52a81ac240f76cb8e6b2b79c71aa49664defde5
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-10 20:51:22 +0300

    Defining routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,56 @@
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import Cookies from "js-cookie";
 
 import Header from "./components/Header/Header";
 import Primary from "./components/Primary/Primary";
 import Secondary from "./components/Secondary/Secondary";
+import NotFound from "./components/Other/NotFound";
 
-import { Auth } from "./api/Server";
+// import { Auth } from "./api/Server";
 
 const App = () => {
     const [user, setUser] = useState(null);
     let activeClassName = "selected";
 
-    // useEffect(() => {
-    //     const fetchUser = async() => {
-    //         let user = await Auth.getUser();
-    //         if (user) setUser(user);
-    //     }
+    useEffect(() => {
+        // const fetchUser = async() => {
+        //     let user = await Auth.getUser();
+        //     if (user) setUser(user);
+        // }
 
-    //     fetchUser();
-    //     console.log(Cookies.get());
-    // });
+        // fetchUser();
+        console.log(Cookies.get());
+    });
 
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     useEffect(() => {
         window.onresize = () => setWindowWidth(window.innerWidth);
     });
 
-    const [view, setView] = useState("primary");
-    // const [main, setMain] = useState(
-    //     <>
-    //         <Primary user={user} activeClassName={activeClassName} />
-    //         <Secondary user={user} setUser={setUser} activeClassName={activeClassName} />
-    //     </>
-    // )
-
-    // useEffect(() => {
-    //     if (windowWidth <= 991) {
-    //         if (view === "primary") setMain(<Primary user={user} activeClassName={activeClassName} />);
-    //         if (view === "secondary") setMain(<Secondary user={user} setUser={setUser} activeClassName={activeClassName} />);
-    //     }
-    // }, [view]);
-
     return (
         <>
-            <Header user={user} setUser={setUser} view={view} setView={setView} windowWidth={windowWidth} />
+            <Header user={user} setUser={setUser} windowWidth={windowWidth} />
             <main>
-                <Primary user={user} activeClassName={activeClassName} />
-                <Secondary user={user} setUser={setUser} activeClassName={activeClassName} />
+                <Routes>
+                    <Route path="/" exact element={<Primary view="menu" user={user} activeClassName={activeClassName} />} />
+                    <Route path="/menu" element={<Primary view="menu" user={user} activeClassName={activeClassName} />} />
+                    {
+                        user ? (
+                            <>
+                                <Route path="/admin" element={<Primary view="admin" user={user} activeClassName={activeClassName} />} />
+                                <Route path="/account" element={<Secondary view="account" user={user} setUser={setUser} activeClassName={activeClassName} />} />
+                                <Route path="/cart" element={<Secondary view="cart" user={user} setUser={setUser} activeClassName={activeClassName} />} />
+                            </>
+                        ) : (
+                            <>
+                                <Route path="/login" element={<Secondary user={user} setUser={setUser} activeClassName={activeClassName} />} />
+                                <Route path="/register" element={<Secondary user={user} setUser={setUser} activeClassName={activeClassName} />} />
+                            </>
+                        )
+                    }
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
             </main>
         </>
     );
