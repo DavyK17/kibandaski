15cd483ca7ec340ef06350279b3719f830509ee2
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-10 19:55:10 +0300

    Fixing URL

--- client/src/api/Server.js
+++ client/src/api/Server.js
@@ -1,4 +1,4 @@
-const url = ""; // process.env.REACT_APP_ENV === "production" ? "https://kibandaski-server.up.railway.app" : "http://localhost:8000";
+const url = "/api";
 
 export const Auth = {
     getUser: async() => {
