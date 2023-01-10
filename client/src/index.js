889ab171639a849259c707ee79b6b1f0372ffd37
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-10 19:54:57 +0300

    Adding Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter as Router } from "react-router-dom";
+
 import App from "./App";
 import "./assets/style/reset.css";
 import "./assets/style/main.scss";
@@ -8,7 +10,9 @@ import "./assets/style/main.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 );
 
