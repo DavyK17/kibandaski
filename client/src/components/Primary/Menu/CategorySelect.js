49c58a71d84b395644befc4c82a27d1cc89ef83c
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-11 20:54:12 +0300

    Creating category select component

---
+++ client/src/components/Primary/Menu/CategorySelect.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { Customer } from "../../../api/Server";
+import capitalise from "../../../util/capitalise";
+
+const CategorySelect = props => {
+    const Server = Customer.products;
+    
+    const { handleChange } = props;
+    const [categories, setCategories] = useState();
+
+    useEffect(() => {
+        const fetchCategories = async() => {
+            let response = await Server.getCategories();
+            if (response) setCategories(response);
+        }
+
+        fetchCategories();
+        // eslint-disable-next-line
+    }, []);
+
+    const renderOptions = () => {
+        if (categories) return categories.map((category, i) => {
+            return <option key={i} value={category}>{capitalise(category)}</option>;
+        });
+    }
+
+    return (
+        <div className="category-select" data-testid="category-select">
+            <label htmlFor="category" className="sr-only">Category</label>
+            <select id="category" onChange={handleChange} defaultValue="all">
+                <option value="all">All items</option>
+                {renderOptions()}
+            </select>
+        </div>
+    )
+}
+
+export default CategorySelect;
