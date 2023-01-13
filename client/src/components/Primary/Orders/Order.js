397e0cddf4567f283a830ee2366fd7541d80df9c
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-13 18:06:13 +0300

    Building out component

--- client/src/components/Primary/Orders/Order.js
+++ client/src/components/Primary/Orders/Order.js
@@ -1,13 +1,31 @@
+import capitalise from "../../../util/capitalise";
+
 const Order = props => {
-    const { id, createdAt, status } = props;
-    
+    const { id, createdAt, status, cancelOrder } = props;
+
+    const renderTime = createdAt => {
+        let options = { day: "numeric", month: "long", year: "numeric", hour: "numeric", minute: "2-digit", hour12: true };
+        let dateTime = new Intl.DateTimeFormat("en-KE", options);
+        return dateTime.format(new Date(createdAt));
+    }
+
+    const cancelButton = <button className="cancel-order" onClick={cancelOrder}>Cancel order</button>;
+
     return (
         <>
-            <p>{id}</p>
-            <p>{createdAt}</p>
-            <p>{status}</p>
+            <div className="item-body">
+                <div className="info">
+                    <p className="name">{renderTime(createdAt)}</p>
+                    <p className="description">View items</p>
+                </div>
+            </div>
+            <div className="item-footer">
+                <p className="id">#{id}</p>
+                <p className="status">{capitalise(status)}</p>
+                {status === "pending" ? cancelButton : null}
+            </div>
         </>
-    );
+    )
 }
 
 export default Order;
