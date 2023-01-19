1edb0c0ea90281f93d2e3a17e106a811ef3bd9f9
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-20 00:06:06 +0300

    Creating Checkout component

---
+++ client/src/components/Secondary/Cart/Checkout.js
@@ -0,0 +1,22 @@
+const Checkout = props => {
+    const { phone, handleBack, handleSubmit } = props;
+
+    const phoneRegex = /^254(11[0-5]|7(([0-2]|9)\d|4([0-6]|8)|5[7-9]|6[8-9]))\d{6}$/;
+    const checkPhone = phone => phone.toString().match(phoneRegex) ? phone : null;
+
+    return (
+        <form className="checkout" onSubmit={handleSubmit} autoComplete="off" data-testid="checkout">
+            <p>Kindly confirm your M-Pesa number:</p>
+            <div className="input">
+                <label className="sr-only" htmlFor="phone">M-Pesa number</label>
+                <input type="tel" id="phone" placeholder="i.e. 254XXXXXXXXX" pattern={phoneRegex.toString().replaceAll("/", "")} defaultValue={checkPhone(phone)} required />
+            </div>
+            <div className="buttons">
+                <button onClick={handleBack}>Back to Cart</button>
+                <button type="submit">Place order</button>
+            </div>
+        </form>
+    )
+}
+
+export default Checkout;
