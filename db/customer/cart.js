872c244d51f6b8b3cbbfb6d148f367a6f3e757dd
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-19 18:52:39 +0300

    Adding payment to checkout flow

--- db/customer/cart.js
+++ db/customer/cart.js
@@ -1,7 +1,15 @@
 // IMPORTS
+const axios = require("axios");
 const pool = require("../pool");
 const idGen = require("../../util/idGen");
 const { isNumeric, isLength, trim } = require("validator");
+const sanitizeHtml = require("../../util/sanitizeHtml");
+
+// M-PESA NUMBER VALIDATOR
+const checkPhone = value => {
+    if (value.match(/^254(11[0-5]|7(([0-2]|9)\d|4([0-6]|8)|5[7-9]|6[8-9]))\d{6}$/)) return true;
+    return false;
+};
 
 // FUNCTIONS
 const getCart = async(req, res) => {
@@ -170,11 +178,18 @@ const emptyCart = async(req, res) => {
     }
 }
 
-const checkout = async(req, res) => {
+const beginCheckout = async(req, res, next) => {
     // Generate order ID
     const orderId = idGen(10);
 
     // VALIDATION AND SANITISATION
+    // Phone number
+    let { phone } = req.body;
+    phone = sanitizeHtml(trim(phone));
+    if (!isNumeric(phone, { no_symbols: true })) return res.status(400).send("Error: Phone must contain numbers only");
+    if (!isLength(phone, { min: 12, max: 12 })) return res.status(400).send("Error: Invalid phone number provided (must be 254XXXXXXXXX).");
+    if (!checkPhone(phone)) return res.status(400).send("Error: Phone must be a Safaricom number");
+
     // User ID
     let userId = trim(req.user.id);
     if (!isNumeric(userId, { no_symbols: true }) || !isLength(userId, { min: 7, max: 7 })) return res.status(401).send("Error: Invalid user ID in session.");
@@ -189,10 +204,95 @@ const checkout = async(req, res) => {
         // Send error if cart is empty
         if (result.rows.length === 0) return res.status(409).send("Error: Your cart is empty.");
 
+        // Create cart item costs array
+        let costs = [];
+
+        // Add cost of each cart item to array
+        let text = "SELECT (products.price * cart_items.quantity) AS total_cost FROM cart_items JOIN products ON products.id = cart_items.product_id WHERE cart_items.cart_id = $1";
+        let values = [cartId];
+        result = await pool.query(text, values);
+        result.rows.forEach(row => costs.push(row.total_cost));
+
+        // Get total cost of all items in cart
+        let totalCost = costs.reduce((a, b) => a + b).toString();
+
+        // Send data to next middleware
+        req.phone = phone;
+        req.orderId = orderId;
+        req.userId = userId;
+        req.cartId = cartId;
+        req.totalCost = totalCost;
+        next();
+    } catch (err) {
+        res.status(500).send("An unknown error occurred. Kindly try again.");
+    }
+}
+
+const getPaymentToken = async(req, res, next) => {
+    const url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+
+    const key = process.env.MPESA_CONSUMER_KEY;
+    const secret = process.env.MPESA_CONSUMER_SECRET;
+    const auth = new Buffer.from(`${key}:${secret}`).toString("base64");
+
+    try {
+        let response = await axios.get(url, { headers: { Authorization: `Basic ${auth}` } });
+        req.accessToken = response.data.access_token;
+        next();
+    } catch (err) {
+        if (err.response) {
+            let { status, data } = err.response;
+            return res.status(status).send(`Error: ${data.errorMessage} (M-Pesa)`);
+        }
+
+        res.status(500).send("An unknown error occurred. Kindly try again.");
+    }
+}
+
+const completeCheckout = async(req, res) => {
+    let { accessToken, phone, orderId, userId, cartId, totalCost } = req;
+    const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+
+    const generateTimestamp = (time = Date.now()) => {
+        let date = new Date(time);
+        const formatDate = date => date.toLocaleString("en-KE", { minimumIntegerDigits: 2, useGrouping: false });
+
+        let year = date.getFullYear();
+        let month = formatDate(date.getMonth() + 1);
+        let day = formatDate(date.getDate());
+        let hours = formatDate(date.getHours());
+        let minutes = formatDate(date.getMinutes());
+        let seconds = formatDate(date.getSeconds());
+
+        return year + month + day + hours + minutes + seconds;
+    }
+
+    let shortcode = 174379;
+    let password = new Buffer.from(`${shortcode}${process.env.MPESA_PASSKEY}${generateTimestamp()}`).toString("base64");
+
+    let body = JSON.stringify({
+        "BusinessShortCode": shortcode,
+        "Password": password,
+        "Timestamp": generateTimestamp(),
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": totalCost,
+        "PartyA": phone,
+        "PartyB": shortcode,
+        "PhoneNumber": phone,
+        "CallBackURL": "https://kibandaski.up.railway.app/api/payment/mpesa-callback",
+        "AccountReference": "Kibandaski App",
+        "TransactionDesc": "Order payment"
+    });
+    let headers = { "Content-Type": "application/json", Authorization: `Bearer ${accessToken}` };
+
+    try {
+        // Make payment
+        await axios.post(url, body, { headers });
+
         // Create order
         let text = `INSERT INTO orders (id, user_id, created_at, status) VALUES ($1, $2, to_timestamp(${Date.now()} / 1000), 'pending') RETURNING id`;
-        let values = [orderId, userId]
-        result = await pool.query(text, values);
+        let values = [orderId, userId];
+        let result = await pool.query(text, values);
 
         // Create order items array
         let items = [];
@@ -212,8 +312,13 @@ const checkout = async(req, res) => {
         // Confirm order
         res.status(201).send(`Order placed with ID: ${orderId}`);
     } catch (err) {
+        if (err.response) {
+            let { status, data } = err.response;
+            return res.status(status).send(`Error: ${data.errorMessage} (M-Pesa)`);
+        }
+
         res.status(500).send("An unknown error occurred. Kindly try again.");
     }
 }
 
-module.exports = { getCart, addToCart, updateCartItem, removeCartItem, emptyCart, checkout };
+module.exports = { getCart, addToCart, updateCartItem, removeCartItem, emptyCart, beginCheckout, getPaymentToken, completeCheckout };
