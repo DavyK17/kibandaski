6a2fc1ec137a4f32d46f302cf8da8ecdde97f951
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-20 15:13:53 +0300

    Building out cancelOrder + reorganising code

--- client/src/components/Primary/Orders/Orders.js
+++ client/src/components/Primary/Orders/Orders.js
@@ -5,57 +5,80 @@ import Order from "./Order";
 import StatusSelect from "./StatusSelect";
 
 import { Customer } from "../../../api/Server";
+import displayErrorMessage from "../../../util/displayErrorMessage";
 
 const Orders = props => {
+    // Destructure props and define server
     const { windowWidth, iconHeight } = props;
     const Server = Customer.orders;
 
+    // STATE + FUNCTIONS
+    // Orders
     const [orders, setOrders] = useState([]);
-    const [items, setItems] = useState([]);
+    const [renderedOrders, setRenderedOrders] = useState([]);
 
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchOrders = async() => {
-            setIsLoading(true);
+    const fetchOrders = async() => {
+        setIsLoading(true);
 
-            try {
-                let orders = await Server.getOrders();
-                if (orders) {
-                    setOrders(orders);
-                    setItems(orders);
-                    setIsLoading(false);
-                }
-            } catch (err) {
-                setError(true);
-                console.log(err);
+        try {
+            let orders = await Server.getOrders();
+            if (orders) {
+                setOrders(orders);
+                setRenderedOrders(orders);
+                setIsLoading(false);
             }
+        } catch (err) {
+            setError(true);
+            console.log(err);
         }
+    }
 
+    useEffect(() => {
         fetchOrders();
         // eslint-disable-next-line
     }, []);
 
+
+    // Order status for which to show orders
     const [status, setStatus] = useState("all");
+    const changeStatus = ({ target }) => setStatus(target.value);
+    
     useEffect(() => {
-        if (status === "all") return setItems(orders);
-        let items = orders.filter(item => item.status === status);
-        setItems(items);
+        if (status === "all") return setRenderedOrders(orders);
+        let list = orders.filter(order => order.status === status);
+        setRenderedOrders(list);
         // eslint-disable-next-line
     }, [status]);
 
-    const changeStatus = ({ target }) => setStatus(target.value);
-
-    const cancelOrder = e => {
-        e.preventDefault();
-    }
-
-    const renderItems = () => {
+    // RENDERING
+    // Orders
+    const renderOrders = () => {
+        // Return skeleton if loading
         if (isLoading) return <Skeleton />;
+
+        // Return error message if error
         if (error) return <p className="error">An error occurred loading your orders. Kindly refresh the page and try again.</p>;
 
-        return items.length === 0 ? <p>No orders to show.</p> : items.map(({ id, createdAt, status }, i) => {
+        // Return message if no orders, otherwise return orders
+        return renderedOrders.length === 0 ? <p>No orders to show.</p> : renderedOrders.map(({ id, createdAt, status }, i) => {
+            // Define function to cancel order
+            const cancelOrder = async e => {
+                e.preventDefault();
+                const status = document.getElementById("status");
+        
+                status.textContent = "Cancelling order…";
+                let response = await Server.cancelOrder(id);
+                if (typeof response === "string") return displayErrorMessage(response);
+        
+                status.textContent = "Order cancelled successfully";
+                fetchOrders();
+                status.textContent = null;
+            }
+
+            // Return order
             return (
                 <li key={i}>
                     <Order id={id} createdAt={createdAt} status={status} windowWidth={windowWidth} iconHeight={iconHeight} cancelOrder={cancelOrder} />
@@ -64,12 +87,13 @@ const Orders = props => {
         });
     };
 
+    // Component
     return (
         <>
             <div className="orders">
                 <StatusSelect handleChange={changeStatus} />
                 <ul>
-                    {renderItems()}
+                    {renderOrders()}
                 </ul>
             </div>
         </>
