2d4f6ef2c34a20deb8c8d3f3dfcdcdc24db98ce6
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-22 17:41:04 +0300

    Removing confirm-federated logic

--- client/src/App.js
+++ client/src/App.js
@@ -59,10 +59,10 @@ const App = () => {
                     {renderRoute(1, "/admin", user ? "admin" : "login")}
                     {renderRoute(1, "/orders", user ? "orders" : "login")}
 
-                    {renderRoute(2, "/account", user ? user.confirmDetails ? "confirm-federated" : "account" : "login")}
-                    {renderRoute(2, "/cart", user ? user.confirmDetails ? "confirm-federated" : "cart" : "login")}
-                    {renderRoute(2, "/login", user ? user.confirmDetails ? "confirm-federated" : "account" : "login")}
-                    {renderRoute(2, "/register", user ? user.confirmDetails ? "confirm-federated" : "account" : "register")}
+                    {renderRoute(2, "/account", user ? "account" : "login")}
+                    {renderRoute(2, "/cart", user ? "cart" : "login")}
+                    {renderRoute(2, "/login", user ? "account" : "login")}
+                    {renderRoute(2, "/register", user ? "account" : "register")}
 
                     <Route path="*" element={<NotFound />} />
                 </Routes>
