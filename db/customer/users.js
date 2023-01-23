1cfd809f30def49f9fe311e0eb2e041f2fb761fd
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-24 00:29:13 +0300

    Minor edit to comments

--- db/customer/users.js
+++ db/customer/users.js
@@ -166,13 +166,13 @@ const unlinkThirdParty = async(req, res) => {
     let userId = trim(req.user.id);
     if (!isNumeric(userId, { no_symbols: true }) || !isLength(userId, { min: 7, max: 7 })) return res.status(401).send("Error: Invalid user ID in session.");
 
-    try { // Get third-party credentials
+    try { // Get third-party credentials from given provider
         let result = await pool.query("SELECT * FROM federated_credentials WHERE user_id = $1 AND provider = $2", [userId, provider]);
 
         // Send error if credentials do not exist
         if (result.rows.length === 0) return res.status(404).send("Error: No credentials found for the given provider.");
 
-        // Delete third-party credentials
+        // Delete third-party credentials from given provider
         result = await pool.query("DELETE FROM federated_credentials WHERE user_id = $1 AND provider = $2", [userId, provider]);
         res.status(204).send(`${provider.charAt(0).toUpperCase() + provider.slice(1)} credentials unlinked successfully.`);
     } catch (err) {
