cbe06c5228dfabe33a30da55f7f6bd4e3be3f4f1
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-23 14:55:14 +0300

    Importing pool

--- util/loginAttempt.js
+++ util/loginAttempt.js
@@ -1,3 +1,6 @@
+// Import database pool
+const pool = require("../db/pool");
+
 // Login attempt log function
 const loginAttempt = async(id, ip, email, strategy, success) => {
     let text = `INSERT INTO login_attempts (id, ip, email, attempted_at, strategy, successful) VALUES ($1, $2, $3, to_timestamp(${Date.now()} / 1000), $4, $5)`;
