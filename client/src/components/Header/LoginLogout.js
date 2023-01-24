d059af81ee88e03cac2c53763fecad653296b287
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-24 14:22:48 +0300

    Showing skeleton when logging out

--- client/src/components/Header/LoginLogout.js
+++ client/src/components/Header/LoginLogout.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+
 import { Auth as Server } from "../../api/Server";
 
 const LoginLogout = props => {
@@ -20,16 +23,20 @@ const LoginLogout = props => {
     )
 
     // Logout
+    let [loggingOut, setLoggingOut] = useState(false);
     const logoutClick = async e => {
         e.preventDefault();
+        setLoggingOut(true);
+
         let response = await Server.logout();
         if (response === "Logout successful") {
             setUser(null);
             navigate("/");
+            setLoggingOut(false);
         };
     }
 
-    const Logout = (
+    const Logout = loggingOut ? <Skeleton /> : (
         <svg id="iconLogout" width={iconHeight} height={iconHeight} viewBox="0 0 24 24" onClick={logoutClick}>
             <path id="pathLogout" style={{ fill:"#ffffff" }} d="M14 19v-.083c-1.178.685-2.542 1.083-4 1.083-4.411 0-8-3.589-8-8s3.589-8 8-8c1.458 0 2.822.398 4 1.083v-2.245c-1.226-.536-2.576-.838-4-.838-5.522 0-10 4.477-10 10s4.478 10 10 10c1.424 0 2.774-.302 4-.838v-2.162zm4-9.592l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z" />
         </svg>
