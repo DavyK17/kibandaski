1177b54d6045e69efe3ef238187b10de9f7b4341
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-25 00:40:39 +0300

    Building out admin navigation component

--- client/src/components/Admin/AdminNav.js
+++ client/src/components/Admin/AdminNav.js
@@ -1,5 +1,29 @@
-const AdminNav = () => {
-    return;
+import { NavLink } from "react-router-dom";
+import capitalise from "../../util/capitalise";
+
+const AdminNav = props => {
+    // Destructure props
+    const { activeClassName } = props;
+
+    // Define function to render menu item
+    const renderMenuItem = name => {
+        return (
+            <li>
+                <NavLink className={({ isActive }) => isActive ? activeClassName : undefined} to={`/admin/${name}`}>{capitalise(name)}</NavLink>
+            </li>
+        )
+    }
+
+    // Return component
+    return (
+        <nav>
+            <ul>
+                {renderMenuItem("orders")}
+                {renderMenuItem("products")}
+                {renderMenuItem("users")}
+            </ul>
+        </nav>
+    )
 }
 
 export default AdminNav;
