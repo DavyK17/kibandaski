5fb91debc159b5bd295bee486175f6193e00aff3
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-25 00:13:11 +0300

    Adding Admin routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Primary from "./components/Primary/Primary";
 import Secondary from "./components/Secondary/Secondary";
+import Admin from "./components/Admin/Admin";
 import NotFound from "./components/Other/NotFound";
 
 import { Auth } from "./api/Server";
@@ -48,6 +49,12 @@ const App = () => {
             } />
         )
 
+        if (type === 3) return (
+            <Route path={path} element={
+                <Admin view={view} activeClassName={activeClassName} iconHeight={iconHeight} ctpr={ctpr} />
+            } />
+        )
+
         throw new Error("No route type provided.");
     }
 
@@ -59,13 +66,19 @@ const App = () => {
                 <Routes>
                     {renderRoute(1, "/", "menu", true)}
                     {renderRoute(1, "/menu", "menu")}
-                    {renderRoute(1, "/admin", user ? "admin" : "login")}
                     {renderRoute(1, "/orders", user ? "orders" : "login")}
 
                     {renderRoute(2, "/account", user ? "account" : "login")}
                     {renderRoute(2, "/cart", user ? "cart" : "login")}
                     {renderRoute(2, "/login", user ? "account" : "login")}
                     {renderRoute(2, "/register", user ? "account" : "register")}
+
+                    <Route path="/admin">
+                        {renderRoute(3, "orders", user ? "orders" : "login")}
+                        {renderRoute(3, "products", user ? "products" : "login")}
+                        {renderRoute(3, "users", user ? "users" : "login")}
+                        {renderRoute(3, "", user ? "orders" : "login")}
+                    </Route>
 
                     <Route path="*" element={<NotFound />} />
                 </Routes>
