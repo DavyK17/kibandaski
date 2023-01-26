e7ae7b6bded203df2d5ca02b68cb97b1866dd1a8
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-26 23:10:29 +0300

    Building out component

--- client/src/components/Admin/Orders/Orders.js
+++ client/src/components/Admin/Orders/Orders.js
@@ -1,5 +1,153 @@
-const Orders = () => {
-    return;
+import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import trim from "validator/lib/trim";
+
+import Order from "./Order";
+import StatusSelect from "../../Primary/Orders/StatusSelect";
+import UserSearch from "./UserSearch";
+
+import { Admin } from "../../../api/Server";
+
+import capitalise from "../../../util/capitalise";
+import displayErrorMessage from "../../../util/displayErrorMessage";
+
+const Orders = props => {
+    // Destructure props and define server
+    const { windowWidth, iconHeight } = props;
+    const Server = Admin.orders;
+
+    // STATE + FUNCTIONS
+    // Orders
+    const [orders, setOrders] = useState([]);
+    const [renderedOrders, setRenderedOrders] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchOrders = async() => {
+        setIsLoading(true);
+
+        try {
+            let orders = await Server.getOrders();
+            if (orders) {
+                setOrders(orders);
+                setRenderedOrders(orders);
+                setIsLoading(false);
+            }
+        } catch (err) {
+            setError(true);
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchOrders();
+        // eslint-disable-next-line
+    }, []);
+
+
+    // Order status for which to show orders
+    const [status, setStatus] = useState("all");
+    const changeStatus = ({ target }) => {
+        setStatus(target.value);
+    };
+    
+    useEffect(() => {
+        let desiredUserId = trim(document.getElementById("search").value);
+
+        if (status === "all") {
+            if (desiredUserId) return setRenderedOrders(orders.filter(order => order.userId === desiredUserId));
+            return setRenderedOrders(orders);
+        }
+
+        let list = orders.filter(order => order.status === status);
+        if (desiredUserId) return setRenderedOrders(list.filter(order => order.userId === desiredUserId));
+        setRenderedOrders(list);
+        // eslint-disable-next-line
+    }, [status]);
+
+    // Define function to show orders by user
+    const showOrdersByUser = e => {
+        e.preventDefault();
+        document.getElementById("order-status").selectedIndex = 0;
+
+        let desiredUserId = trim(e.target[0].value);
+        if (!desiredUserId) return setRenderedOrders(orders);
+
+        let userOrders = orders.filter(order => order.userId === desiredUserId);
+        setRenderedOrders(userOrders);
+    }
+
+    // RENDERING
+    // Orders
+    const renderOrders = () => {
+        // Return error message if error
+        if (error) return <p className="error">An error occurred loading your orders. Kindly refresh the page and try again.</p>;
+
+        // Return skeleton if loading
+        if (isLoading) return <Skeleton />;
+
+        // Return message if no orders, otherwise return orders
+        return renderedOrders.length === 0 ? <p>No orders to show.</p> : renderedOrders.map(({ id, userId, createdAt, status }, i) => {
+            // Define function to acknowledge or fulfill order
+            const orderAction = type => {
+                const logic = (acknowledge, fulfill) => type === 1 ? acknowledge : type === 2 ? fulfill : null;
+
+                let statusText = logic("Acknowledging order…", "Fulfilling order…");
+                let desiredResponse = logic("Order acknowledged", "Order fulfilled");
+
+                return async e => {
+                    e.preventDefault();
+    
+                    const mainStatus = document.getElementById("status");
+                    const orderStatus = document.getElementById(`order-${id}-status`);
+    
+                    mainStatus.textContent = null;
+                    orderStatus.textContent = statusText;
+    
+                    let response = await logic(Server.acknowledgeOrder(id), Server.fulfillOrder(id));
+                    if (!response.includes(desiredResponse)) {
+                        displayErrorMessage(response);
+                        orderStatus.textContent = "Error";
+                    } else {
+                        fetchOrders();
+
+                        setStatus("all");
+                        document.getElementById("order-status").selectedIndex = 0;
+                        document.getElementsByClassName("user-search")[0].reset();
+                    }
+    
+                    setTimeout(() => orderStatus.textContent = capitalise(status), 3000);
+                }
+            }
+
+            // Return order
+            return (
+                <li key={i}>
+                    <Order
+                        details={{ id, userId, createdAt, status }} windowWidth={windowWidth} iconHeight={iconHeight}
+                        acknowledgeOrder={orderAction(1)} fulfillOrder={orderAction(2)}
+                    />
+                </li>
+            )
+        });
+    };
+
+    // Component
+    return (
+        <>
+            <div className="orders">
+                <div className="sort">
+                    <StatusSelect handleChange={changeStatus} />
+                    <UserSearch handleSubmit={showOrdersByUser} />
+                    <p id="status"></p>
+                </div>
+                <ul>
+                    {renderOrders()}
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default Orders;
