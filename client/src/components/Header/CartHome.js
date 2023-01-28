da634da1b76b88b71c3fc4b4598ee35ca0955c61
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-28 21:16:36 +0300

    Moving handler functions to Header

--- client/src/components/Header/CartHome.js
+++ client/src/components/Header/CartHome.js
@@ -1,49 +1,23 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
 
 const CartHome = props => {
     // Destructure props
-    const { iconHeight, user } = props;
+    const { user, iconHeight, location, checkPathFor, handleClick } = props;
 
-    // Define useLocation() and useNavigate(0)
-    let location = useLocation();
-    let navigate = useNavigate();
-
-    // Define primary and secondary paths
-    const primary = ["/", "/menu", "/orders"];
-    const secondary = ["/login", "/register", "/account", "/cart"];
-    const admin = ["/admin/orders", "/admin/products", "/admin/users"];
-
-    // Define function to check whether current path is primary or secondary
-    const checkPathFor = type => {
-        let current;
-        if (type === "primary") current = primary;
-        if (type === "secondary") current = secondary;
-        if (type === "admin") current = admin;
-
-        current = current.filter(pathname => location.pathname === pathname);
-        return current.length > 0 ? true : false;
-    }
-
-    // Define icons
+    // ICONS
+    // Cart
     const Cart = (
         <svg className="iconCart" width={iconHeight} height={iconHeight} viewBox="0 0 24 24">
             <path className="pathCart" style={{ fill:"#ffffff" }} d="M4.558 7l4.701-4.702c.199-.198.46-.298.721-.298.613 0 1.02.505 1.02 1.029 0 .25-.092.504-.299.711l-3.26 3.26h-2.883zm12.001 0h2.883l-4.701-4.702c-.199-.198-.46-.298-.721-.298-.613 0-1.02.505-1.02 1.029 0 .25.092.504.299.711l3.26 3.26zm3.703 4l-.016.041-3.598 8.959h-9.296l-3.597-8.961-.016-.039h16.523zm3.738-2h-24v2h.643c.534 0 1.021.304 1.256.784l4.101 10.216h12l4.102-10.214c.233-.481.722-.786 1.256-.786h.642v-2z" />
         </svg>
     )
 
+    // Home
     const Home = (
         <svg className="iconHome" width={iconHeight} height={iconHeight} viewBox="0 0 24 24">
             <path className="pathHome" style={{ fill:"#ffffff" }}  d="M12 9.185l7 6.514v6.301h-3v-5h-8v5h-3v-6.301l7-6.514zm0-2.732l-9 8.375v9.172h7v-5h4v5h7v-9.172l-9-8.375zm12 5.695l-12-11.148-12 11.133 1.361 1.465 10.639-9.868 10.639 9.883 1.361-1.465z" />
         </svg>
     )
-
-    // Define click handler
-    const handleClick = e => {
-        e.preventDefault();
-        if ((e.target.classList[0] === "iconCart" || e.target.classList[0] === "pathCart") && checkPathFor("primary") && user) navigate("/cart");
-        if ((e.target.classList[0] === "iconHome" || e.target.classList[0] === "pathHome") && (checkPathFor("secondary") || checkPathFor("admin"))) navigate("/");
-    }
     
     // Set state
     let [icon, setIcon] = useState(user ? (checkPathFor("primary") ? Cart : Home) : Home);
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,64 @@
+import { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
 import CartHome from "./CartHome";
 import Logo from "./Logo";
 import LoginLogout from "./LoginLogout";
 
+import { Auth as Server } from "../../api/Server";
+
 const Header = props => {
     // Destructure props
     const { user, setUser, windowWidth, iconHeight } = props;
+
+    // Define useLocation() and useNavigate(0)
+    let location = useLocation();
+    let navigate = useNavigate();
+
+    // Define primary, secondary and admin paths
+    const primary = ["/", "/menu", "/orders"];
+    const secondary = ["/login", "/register", "/account", "/cart"];
+    const admin = ["/admin/orders", "/admin/products", "/admin/users"];
+
+    // Define function to check whether current path is primary or secondary
+    const checkPathFor = type => {
+        let current;
+        if (type === "primary") current = primary;
+        if (type === "secondary") current = secondary;
+        if (type === "admin") current = admin;
+
+        current = current.filter(pathname => location.pathname === pathname);
+        return current.length > 0 ? true : false;
+    }
+
+    // CLICK HANDLERS
+    // Cart/Home
+    const cartHomeClick = e => {
+        e.preventDefault();
+        if ((e.target.classList[0] === "iconCart" || e.target.classList[0] === "pathCart") && checkPathFor("primary") && user) navigate("/cart");
+        if ((e.target.classList[0] === "iconHome" || e.target.classList[0] === "pathHome") && (checkPathFor("secondary") || checkPathFor("admin"))) navigate("/");
+    }
+
+    // Login
+    const loginClick = async e => {
+        e.preventDefault();
+        navigate("/login");
+    }
+
+    // Logout
+    let [loggingOut, setLoggingOut] = useState(false);
+    const logoutClick = async e => {
+        e.preventDefault();
+        setLoggingOut(true);
+
+        let response = await Server.logout();
+        if (response === "Logout successful") {
+            setUser(null);
+            navigate("/");
+            setLoggingOut(false);
+        }
+    }
+
 
     // Return component
     return (
@@ -13,13 +67,13 @@ const Header = props => {
             <nav>
                 <ul>
                     <li>
-                        <CartHome iconHeight={iconHeight} user={user} />
+                        <CartHome user={user} iconHeight={iconHeight} location={location} checkPathFor={checkPathFor} handleClick={cartHomeClick} />
                     </li>
                     <li id="logo">
                         <Logo windowWidth={windowWidth} />
                     </li>
                     <li>
-                        <LoginLogout iconHeight={iconHeight} user={user} setUser={setUser} />
+                        <LoginLogout user={user} iconHeight={iconHeight} loggingOut={loggingOut} handleLogin={loginClick} handleLogout={logoutClick} />
                     </li>
                 </ul>
             </nav>
