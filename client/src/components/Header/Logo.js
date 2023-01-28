7fea1fc047f4d215e52fa3d15fef371ada7c25cb
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-28 22:18:54 +0300

    Adding test IDs

--- client/src/components/Header/Logo.js
+++ client/src/components/Header/Logo.js
@@ -7,7 +7,7 @@ const Logo = props => {
     
     // Return component
     return (
-        <a href="https://github.com/DavyK17/kibandaski-client" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/DavyK17/kibandaski-client" target="_blank" rel="noopener noreferrer" data-testid="logo">
             <img src={windowWidth > 991 ? Wordmark : Icon} alt={windowWidth > 991 ? "Kibandaski wordmark" : "Kibandaski logo"} height="40" />
         </a>
     )
