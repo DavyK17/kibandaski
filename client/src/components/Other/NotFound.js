e13c05f94b4307c91600c6eb6595dcf02b8d721d
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-28 18:33:32 +0300

    Building out component

--- client/src/components/Other/NotFound.js
+++ client/src/components/Other/NotFound.js
@@ -1,5 +1,23 @@
+import { useNavigate, useLocation } from "react-router-dom";
+
 const NotFound = () => {
-    return;
+    // Define useNavigate() and useLocation()
+    let navigate = useNavigate();
+    let location = useLocation();
+
+    // Define function to go back to previous page
+    const goBack = e => {
+        e.preventDefault();
+        navigate(-1);
+    }
+
+    // Return component
+    return (
+        <section className="not-found">
+            <h2>Not Found</h2>
+            <p>The page you requested <span className="path">({location.pathname})</span> does not exist. <a href="/#" onClick={goBack}>Click here</a> to go back to the previous page.</p>
+        </section>
+    );
 }
 
 export default NotFound;
