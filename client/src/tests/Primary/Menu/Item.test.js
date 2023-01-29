3aee3105d2c2fe1bfb9d14db2d7578b0e3190918
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-30 01:31:45 +0300

    Minor test description edit

--- client/src/tests/Primary/Menu/Item.test.js
+++ client/src/tests/Primary/Menu/Item.test.js
@@ -3,13 +3,13 @@ import Item from "../../../components/Primary/Menu/Item";
 import { products } from "../../util/dataMock";
 
 describe("Menu item", () => {
-    test("calls addToCart when cart icon is clicked", () => {
+    test("calls addToCart when button is clicked", () => {
         const clickMock = jest.fn();
 
         const { getByRole } = render(<Item details={products[0]} addToCart={clickMock} />);
         let button = getByRole("button");
 
         fireEvent.click(button);
-        expect(clickMock).toBeCalled(); 
+        expect(clickMock).toBeCalled();
     });
 });
