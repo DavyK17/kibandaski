2e189ab9c014060a41c3704ec6164f4d5d831e3c
Davy Kamanzi <davykamanzi@gmail.com>
2023-01-31 01:25:36 +0300

    Adding Primary tests

---
+++ client/src/tests/Primary/Primary.test.js
@@ -0,0 +1,55 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import Primary from "../../components/Primary/Primary";
+
+describe("Primary section", () => {
+    describe("Third-party registration confirmed", () => {
+        beforeEach(() => {
+            render(
+                <Router>
+                    <Primary view="menu" ctpr={false} />
+                </Router>
+            );
+        });
+    
+        test("renders heading", () => {
+            let heading = screen.getByRole("heading");
+            expect(heading).toBeInTheDocument();
+        });
+    
+        test("renders navigation", () => {
+            let nav = screen.getByRole("navigation");
+            expect(nav).toBeInTheDocument();
+        });
+    
+        test("renders view component", () => {
+            let component = screen.getByTestId("menu");
+            expect(component).toBeInTheDocument();
+        });
+    });
+
+    describe("Third-party registration not confirmed", () => {
+        beforeEach(() => {
+            render(
+                <Router>
+                    <Primary view="menu" ctpr={true} />
+                </Router>
+            );
+        });
+    
+        test("does not render heading", () => {
+            let heading = screen.queryByRole("heading");
+            expect(heading).not.toBeInTheDocument();
+        });
+    
+        test("does not render navigation", () => {
+            let nav = screen.queryByRole("navigation");
+            expect(nav).not.toBeInTheDocument();
+        });
+    
+        test("does not render view component", () => {
+            let component = screen.queryByTestId("menu");
+            expect(component).not.toBeInTheDocument();
+        });
+    });
+});
