b1a9af4bc320a2e42c4ebedcd5478c424ec75a85
Davy Kamanzi <davykamanzi@gmail.com>
2023-03-09 13:36:26 +0300

    Using isAuthenticated

--- server/middleware/authenticated.js
+++ server/middleware/authenticated.js
@@ -1,4 +1,4 @@
-const loggedIn = (req, res, next) => req.user ? next() : res.status(401).send("Error: You are not logged in.");
-const loggedOut = (req, res, next) => !req.user ? next() : res.status(403).send("Error: You are not logged out.");
+const loggedIn = (req, res, next) => req.isAuthenticated() ? next() : res.status(401).send("Error: You are not logged in.");
+const loggedOut = (req, res, next) => !req.isAuthenticated() ? next() : res.status(403).send("Error: You are not logged out.");
 
 module.exports = { loggedIn, loggedOut };
