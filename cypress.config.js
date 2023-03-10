bcb4d1856352b1f85452c28f31bf174c108bb8fd
Davy Kamanzi <davykamanzi@gmail.com>
2023-03-10 15:16:45 +0300

    Fixing baseUrl and enable Cypress Studio

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,8 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
-        baseUrl: "http://localhost:3000",
-        specPattern: "src/tests/e2e/**/*.cy.{js,jsx,ts,tsx}"
+        baseUrl: "http://localhost:8000",
+        specPattern: "src/tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
+        experimentalStudio: true
     },
 });
